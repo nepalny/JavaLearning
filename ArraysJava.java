41e77033cd8545224d5eee732fee51afcee92c2e
nepalny <53278546+nepalny@users.noreply.github.com>
2019-07-24 17:10:37 -0400

    Create ArraysJava.java

---
+++ ArraysJava.java
@@ -0,0 +1,35 @@
+package javaarrays;
+
+import java.util.Arrays;
+
+public class ArraysJava {
+	public static void main(String[] args) {
+
+		int[] num = new int[5];
+		num[0] = 5;
+		num[1] = 4;
+		num[2] = 77;
+		num[3] = 9;
+		num[4] = 2;
+
+		System.out.println(Arrays.toString(num));
+
+		String[] name = new String[3];
+		name[0] = "nepal";
+		name[1] = "azad";
+		name[2] = "biju";
+
+		System.out.println(Arrays.toString(name));
+
+		char[] upperCase = new char[5];
+		upperCase[0] = 'A';
+		upperCase[1] = 'B';
+		upperCase[2] = 'C';
+		upperCase[3] = 'D';
+		upperCase[4] = 'E';
+
+		System.out.println(Arrays.toString(upperCase));
+
+	}
+
+}
